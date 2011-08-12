98361b3beb57241c4944e735b0745fd9630734a9
Joe Sauve <jsauve@novaitservices.com>
2011-08-12 13:19:19 -0500

    Commit initial batch of files. Current progress: 12.7%

---
+++ AASharp/AAS2DCoordinate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AASharp
+{
+    public class AAS2DCoordinate
+    {
+        public AAS2DCoordinate() { }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
---
+++ AASharp/AASMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AASharp
+{
+    public static class AASMath
+    {
+        // equivalent of modf in C++
+        public static double modF(double source, ref double integralPart)
+        {
+            return source - (integralPart = Math.Floor(source));
+        }
+    }
+}
