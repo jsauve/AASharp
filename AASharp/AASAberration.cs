73b9915840d79e4a11f9f22c95460edc0004832c
Joe Sauve <jsauve@novaitservices.com>
2011-08-19 02:16:30 -0500

    Many many new files completed. All planets are currently stubbed out with stubbed methods. Completion: 41.27%

---
+++ AASharp/AASAberration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AASharp
+{
+    public static class AASAberration
+    {
+        public static AAS2DCoordinate EclipticAberration(double apparentGeocentricLongitude, double apparentGeocentricLatitude, double jD)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
