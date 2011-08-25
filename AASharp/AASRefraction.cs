81861c6f62409aaeafedb9051e0fc2b3ea1a4e28
Joe Sauve <jsauve@novaitservices.com>
2011-08-25 02:25:39 -0500

    MANY files completed. 95% complete.

---
+++ AASharp/AASRefraction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AASharp
+{
+    public static class AASRefraction
+    {
+        public static double AASRefractionRefractionFromApparent(double Altitude, double Pressure, double Temperature)
+        {
+            //return a constant value from this method if the altitude is below a specific value
+            if (Altitude <= -1.6962987799993996)
+                Altitude = -1.6962987799993996;
+
+            double value = 1 / (Math.Tan(AASCoordinateTransformation.DegreesToRadians(Altitude + 7.31 / (Altitude + 4.4)))) + 0.0013515;
+            value *= (Pressure / 1010 * 283 / (273 + Temperature));
+            value /= 60;
+            return value;
+        }
+
+        public static double AASRefractionRefractionFromTrue(double Altitude, double Pressure, double Temperature)
+        {
+            //return a constant value from this method if the altitude is below a specific value
+            if (Altitude <= -1.9006387000003735)
+                Altitude = -1.9006387000003735;
+
+            double value = 1.02 / (Math.Tan(AASCoordinateTransformation.DegreesToRadians(Altitude + 10.3 / (Altitude + 5.11)))) + 0.0019279;
+            value *= (Pressure / 1010 * 283 / (273 + Temperature));
+            value /= 60;
+            return value;
+        }
+    }
+}
