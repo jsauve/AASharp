27d857a6036066b23b4134e03ea5454976cd6193
jsauve <jsauve@novaitservices.com>
2011-08-25 12:22:12 -0500

    Started SaturnRings. Checking in so I can pull down.

---
+++ AASharp/AASSaturnRings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AASharp
+{
+    public class AASSaturnRingDetails
+    {
+        double B;
+        double Bdash;
+        double P;
+        double a;
+        double b;
+        double DeltaU;
+    }
+
+    public static class AASSaturnRings
+    {
+    }
+}
