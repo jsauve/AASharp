03034ea11c7d832dc4c64fe0577ea378808d1a5e
Julien Tschäppät <git@elendil.software>
2017-12-07 07:29:28 +0100

    Remove redundant constructors

--- Src/AASharp/AAS2DCoordinate.cs
+++ Src/AASharp/AAS2DCoordinate.cs
@@ -2,10 +2,6 @@ namespace AASharp
 {
     public class AAS2DCoordinate
     {
-        public AAS2DCoordinate()
-        {
-        }
-
         public double X { get; set; }
         public double Y { get; set; }
     }
