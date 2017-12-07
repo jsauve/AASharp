9ee80f57db4542f4776a2dd0edcd4e5f6307ffda
Julien Tschäppät <git@elendil.software>
2017-12-07 07:16:18 +0100

    Remove unused directives

--- Src/AASharp/AASJewishCalendar.cs
+++ Src/AASharp/AASJewishCalendar.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace AASharp
+﻿namespace AASharp
 {
     public static class AASJewishCalendar
     {
--- Src/AASharp/AASSolarEclipseDetails.cs
+++ Src/AASharp/AASSolarEclipseDetails.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace AASharp
 {
     public class AASSolarEclipseDetails
