568766a31f21b1655e32421e5cc091a9c6d1e9f3
Julien Tschäppät <git@elendil.software>
2018-01-11 07:12:24 +0100

    Unit tests for AASKepler

---
+++ Test/AASharp.Tests/KeplerTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace AASharp.Tests
+{
+    public class KeplerTest
+    {
+        [Theory]
+        [InlineData(5, 0.1, 100, 5.5545892538723143)]
+        [InlineData(5, 0.9, 100, 33.344446958990886)]
+        public void CalculateTest(double M, double e, int nIterations, double expectedResult)
+        {
+            double e0 = AASKepler.Calculate(M, e, nIterations);
+            Assert.Equal(expectedResult, e0);
+        }
+    }
+}
