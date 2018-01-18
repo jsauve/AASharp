fd4ee2d23ec7fa527d89d5deb9d82f42cb93fd7a
Julien Tschäppät <git@elendil.software>
2018-01-18 07:40:46 +0100

    Unit tests for AASEquationOfTime

---
+++ Test/AASharp.Tests/EquationOfTimeTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace AASharp.Tests
+{
+    public class EquationOfTimeTest
+    {
+        [Theory]
+        [InlineData(2448908.5, false, 13.709950290402466)]
+        public void CalculateTest(double jd, bool highPrecision, double expectedResult)
+        {
+            double e = AASEquationOfTime.Calculate(jd, highPrecision);
+            Assert.Equal(expectedResult, e);
+        }
+    }
+}
