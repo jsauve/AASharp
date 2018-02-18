350be01eded0499bb43bb1f61264994b04c33ee6
Julien Tschäppät <git@elendil.software>
2018-02-18 14:57:06 +0100

    Updated unit tests after checking AA++ test application

--- Test/AASharp.Tests/CoordinateTransformationTest.cs
+++ Test/AASharp.Tests/CoordinateTransformationTest.cs
@@ -59,7 +59,80 @@ namespace AASharp.Tests
             Assert.Equal(23.1546225, alpha2);
         }
 
+        [Theory]
+        [InlineData(-7, 5.5663706143591725)]
+        [InlineData(-1, 5.2831853071795862)]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(7, 0.71681469282041377)]
+        public void MapTo0To2PIRangeTest(double angle, double expectedValue)
+        {
+            double mappedValue = AASCoordinateTransformation.MapTo0To2PIRange(angle);
+            Assert.Equal(expectedValue, mappedValue);
+        }
 
-        
+        [Theory]
+        [InlineData(-361, 359)]
+        [InlineData(-150, 210)]
+        [InlineData(-50, 310)]
+        [InlineData(50, 50)]
+        [InlineData(150, 150)]
+        [InlineData(250, 250)]
+        [InlineData(370, 10)]
+        [InlineData(770, 50)]
+        public void MapTo0To360RangeTest(double degrees, double expectedValue)
+        {
+            double mappedValue = AASCoordinateTransformation.MapTo0To360Range(degrees);
+            Assert.Equal(expectedValue, mappedValue);
+        }
+
+        [Theory]
+        [InlineData(-26, 22)]
+        [InlineData(-15, 9)]
+        [InlineData(-5, 19)]
+        [InlineData(5, 5)]
+        [InlineData(15, 15)]
+        [InlineData(25, 1)]
+        [InlineData(37, 13)]
+        [InlineData(77, 5)]
+        public void MapTo0To24RangeTest(double hourAngle, double expectedValue)
+        {
+            double mappedValue = AASCoordinateTransformation.MapTo0To24Range(hourAngle);
+            Assert.Equal(expectedValue, mappedValue);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0.1, 0.10000000000000001)]
+        [InlineData(1, 1)]
+        [InlineData(7, 7)]
+        [InlineData(40, 40)]
+        [InlineData(87, 87)]
+        [InlineData(90, 90)]
+        [InlineData(170, 10)]
+        [InlineData(180, 0)]
+        [InlineData(185, -5)]
+        [InlineData(269, -89)]
+        [InlineData(272, -88)]
+        [InlineData(360, 0)]
+        [InlineData(361, 1)]
+        [InlineData(-361, -1)]
+        [InlineData(-360, 0)]
+        [InlineData(-272, 88)]
+        [InlineData(-269, 89)]
+        [InlineData(-185, 5)]
+        [InlineData(-180, 0)]
+        [InlineData(-170, -10)]
+        [InlineData(-97, -83)]
+        [InlineData(-90, -90)]
+        [InlineData(-87, -87)]
+        [InlineData(-40, -40)]
+        [InlineData(-7, -7)]
+        [InlineData(-1, -1)]
+        public void MapToMinus90To90RangeTest(double degrees, double expectedValue)
+        {
+            double mappedValue = AASCoordinateTransformation.MapToMinus90To90Range(degrees);
+            Assert.Equal(expectedValue, mappedValue);
+        }
     }
 }
--- Test/AASharp.Tests/PhysicalSunTest.cs
+++ Test/AASharp.Tests/PhysicalSunTest.cs
@@ -6,6 +6,8 @@ namespace AASharp.Tests
     {
         [Theory]
         [InlineData(2448908.50068, false, 5.9882130001199663, 238.6316689148353, 26.274011444642024)]
+        [InlineData(2451545.5, false, -3.0711457698488323, 354.87401230329306, 1.8918344621229075)]
+        [InlineData(2451910.5, false, -3.0415292200986666, 217.31182640670187, 2.0089019889582511)]
         public void CalculateTest(double jd, bool highPrecision, double expectedB0, double expectedL0, double expectedP)
         {
             AASPhysicalSunDetails psd = AASPhysicalSun.Calculate(jd, highPrecision);
