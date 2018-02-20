1e851d5fbd63fd34b90a1b7279266b1e9f3a1e87
Julien Tschäppät <git@elendil.software>
2018-02-20 07:08:41 +0100

    Added High Precision test for AASMars

--- Test/AASharp.Tests/MarsTest.cs
+++ Test/AASharp.Tests/MarsTest.cs
@@ -6,6 +6,7 @@ namespace AASharp.Tests
     {
         [Theory]
         [InlineData(2448935.500683, false, 78.473820271893032)]
+        [InlineData(2448935.500683, true, 78.47378481953119)]
         public void EclipticLongitudeTest(double JD, bool bHighPrecision, double expectedLongitude)
         {
             double longitude = AASMars.EclipticLongitude(JD, bHighPrecision);
@@ -14,6 +15,7 @@ namespace AASharp.Tests
 
         [Theory]
         [InlineData(2448935.500683, false, 0.89640101634757841)]
+        [InlineData(2448935.500683, true, 0.89632892538680353)]
         public void EclipticLatitudeTest(double JD, bool bHighPrecision, double expectedEclipticLatitude)
         {
             double eclipticLatitude = AASMars.EclipticLatitude(JD, bHighPrecision);
@@ -22,6 +24,7 @@ namespace AASharp.Tests
 
         [Theory]
         [InlineData(2448935.500683, false, 1.5416610439393872)]
+        [InlineData(2448935.500683, true, 1.5416593348419347)]
         public void RadiusVectorTest(double JD, bool bHighPrecision, double expectedRadiusVector)
         {
             double radiusVector = AASMars.RadiusVector(JD, bHighPrecision);
