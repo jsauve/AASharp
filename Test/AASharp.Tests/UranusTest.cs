57f3c1ba321acb3475cda7fe01fe5f6c28488262
Julien Tschäppät <git@elendil.software>
2018-02-20 07:27:53 +0100

    Added High Precision test for AASUranus

--- Test/AASharp.Tests/UranusTest.cs
+++ Test/AASharp.Tests/UranusTest.cs
@@ -5,7 +5,8 @@ namespace AASharp.Tests
     public class UranusTest
     {
         [Theory]
-        [InlineData(2448976.5, false, 287.890891566249)]
+        [InlineData(2448976.5, false, 287.89089156624902)]
+        [InlineData(2448976.5, true, 287.89104181385028)]
         public void EclipticLongitudeTest(double JD, bool bHighPrecision, double expectedLongitude)
         {
             double longitude = AASUranus.EclipticLongitude(JD, bHighPrecision);
@@ -14,6 +15,7 @@ namespace AASharp.Tests
 
         [Theory]
         [InlineData(2448976.5, false, -0.43064206021268953)]
+        [InlineData(2448976.5, true, -0.4306637836173195)]
         public void EclipticLatitudeTest(double JD, bool bHighPrecision, double expectedEclipticLatitude)
         {
             double eclipticLatitude = AASUranus.EclipticLatitude(JD, bHighPrecision);
@@ -22,6 +24,7 @@ namespace AASharp.Tests
 
         [Theory]
         [InlineData(2448976.5, false, 19.570256184055342)]
+        [InlineData(2448976.5, true, 19.570237704842476)]
         public void RadiusVectorTest(double JD, bool bHighPrecision, double expectedRadiusVector)
         {
             double radiusVector = AASUranus.RadiusVector(JD, bHighPrecision);
