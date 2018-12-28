a053f34fec8427bce6d970ed4e2e2a5ea795ecc8
Julien Tschäppät <git@elendil.software>
2018-12-28 17:21:20 +0100

    Code cleanup

--- Src/AASharp/Constellations/CoordinatesToConstellation.cs
+++ Src/AASharp/Constellations/CoordinatesToConstellation.cs
@@ -23,51 +23,46 @@ namespace AASharp.Constellations
     {
         private static (double ra, double dec) Precess(double ra1, double dec1, double epoch1, double epoch2)
         {
-            double cdr, csr;
-            double[] x1 = {0.0, 0.0, 0.0};
             double[] x2 = {0.0, 0.0, 0.0};
-            double [][] r = {new[]{0.0, 0.0, 0.0}, new[]{0.0, 0.0, 0.0}, new[]{0.0, 0.0, 0.0}}; 
-            double t, st, a, b, c, sina, sinb, sinc, cosa, cosb, cosc, ra2, dec2;
+            double[][] r = {new[] {0.0, 0.0, 0.0}, new[] {0.0, 0.0, 0.0}, new[] {0.0, 0.0, 0.0}};
 
-            cdr = Math.PI / 180.0;
-            csr = cdr / 3600.0;
-            a = Math.Cos(dec1);
-            x1[0] = a * Math.Cos(ra1);
-            x1[1] = a * Math.Sin(ra1);
-            x1[2] = Math.Sin(dec1);
-            
-            t = 0.001 * (epoch2 - epoch1);
-            st = 0.001 * (epoch1 - 1900.0);
+            double cdr = Math.PI / 180.0;
+            double csr = cdr / 3600.0;
+            double a = Math.Cos(dec1);
+            double[] x1 = {a * Math.Cos(ra1), a * Math.Sin(ra1), Math.Sin(dec1)};
+
+            double t = 0.001 * (epoch2 - epoch1);
+            double st = 0.001 * (epoch1 - 1900.0);
             a = csr * t * (23042.53 + st * (139.75 + 0.06 * st) + t * (30.23 - 0.27 * st + 18.0 * t));
-            b = csr * t * t * (79.27 + 0.66 * st + 0.32 * t) + a;
-            c = csr * t * (20046.85 - st * (85.33 + 0.37 * st) + t * (-42.67 - 0.37 * st - 41.8 * t));
-            sina = Math.Sin(a);
-            sinb = Math.Sin(b);
-            sinc = Math.Sin(c);
-            cosa = Math.Cos(a);
-            cosb = Math.Cos(b);
-            cosc = Math.Cos(c);
-            r[0][0] = cosa* cosb*cosc - sina* sinb;
-            r[0][1] = -cosa* sinb - sina* cosb*cosc;
-            r[0][2] = -cosb* sinc;
-            r[1][0] = sina* cosb + cosa* sinb*cosc;
-            r[1][1] = cosa* cosb - sina* sinb*cosc;
-            r[1][2] = -sinb* sinc;
-            r[2][0] = cosa* sinc;
-            r[2][1] = -sina* sinc;
+            double b = csr * t * t * (79.27 + 0.66 * st + 0.32 * t) + a;
+            double c = csr * t * (20046.85 - st * (85.33 + 0.37 * st) + t * (-42.67 - 0.37 * st - 41.8 * t));
+            double sina = Math.Sin(a);
+            double sinb = Math.Sin(b);
+            double sinc = Math.Sin(c);
+            double cosa = Math.Cos(a);
+            double cosb = Math.Cos(b);
+            double cosc = Math.Cos(c);
+            r[0][0] = cosa * cosb * cosc - sina * sinb;
+            r[0][1] = -cosa * sinb - sina * cosb * cosc;
+            r[0][2] = -cosb * sinc;
+            r[1][0] = sina * cosb + cosa * sinb * cosc;
+            r[1][1] = cosa * cosb - sina * sinb * cosc;
+            r[1][2] = -sinb * sinc;
+            r[2][0] = cosa * sinc;
+            r[2][1] = -sina * sinc;
             r[2][2] = cosc;
             for (var i = 0; i < 3; i++)
             {
                 x2[i] = r[i][0] * x1[0] + r[i][1] * x1[1] + r[i][2] * x1[2];
             }
 
-            ra2 = Math.Atan2(x2[1], x2[0]);
+            double ra2 = Math.Atan2(x2[1], x2[0]);
             if (ra2 < 0.0)
             {
                 ra2 += 2.0 * Math.PI;
             }
 
-            dec2 = Math.Asin(x2[2]);
+            double dec2 = Math.Asin(x2[2]);
             return (ra2, dec2);
         }
 
