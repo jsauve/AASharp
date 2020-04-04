e2daee868911faafef4ef2cda99f9ae5d3739431
Julien Tschäppät <git@elendil.software>
2020-04-04 22:51:27 +0200

    Removed ELP2000 references

--- Src/AASharp/AASMoonPhases2.cs
+++ Src/AASharp/AASMoonPhases2.cs
@@ -7,12 +7,7 @@ namespace AASharp
     {
         public enum Algorithm
         {
-            MeeusTruncated = 0,
-            ELP2000 = 1,
-            ELPMPP02Nominal = 2,
-            ELPMPP02LLR = 3,
-            ELPMPP02DE405 = 4,
-            ELPMPP02DE406 = 5
+            MeeusTruncated = 0
         }
 
         public static List<AASMoonPhasesDetails2> Calculate(double StartJD, double EndJD, double StepInterval = 0.007,
@@ -33,32 +28,6 @@ namespace AASharp
                     case Algorithm.MeeusTruncated:
                         ExcessApparentGeocentricLongitude = AASCoordinateTransformation.MapTo0To360Range(AASMoon.EclipticLongitude(JD) - AASSun.ApparentEclipticLongitude(JD, false));
                         break;
-
-                    //case Algorithm.ELP2000:
-                    //     MoonPos = AASPrecession.PrecessEcliptic(AASELP2000.EclipticLongitude(JD), AASELP2000.EclipticLatitude(JD), 2451545.0, JD);
-                    //        ExcessApparentGeocentricLongitude = AASCoordinateTransformation.MapTo0To360Range(MoonPos.X - AASSun.ApparentEclipticLongitude(JD, true));
-                    //        break;
-
-                    //case Algorithm.ELPMPP02Nominal:
-                    //     MoonPos = AASPrecession.PrecessEcliptic(AASELPMPP02.EclipticLongitude(JD, AASELPMPP02.Correction.Nominal), AASELPMPP02.EclipticLatitude(JD, AASELPMPP02.Correction.Nominal), 2451545.0, JD);
-                    //        ExcessApparentGeocentricLongitude = AASCoordinateTransformation.MapTo0To360Range(MoonPos.X - AASSun.ApparentEclipticLongitude(JD, true));
-                    //        break;
-                    
-                    //case Algorithm.ELPMPP02LLR:
-                    //     MoonPos = AASPrecession.PrecessEcliptic(AASELPMPP02.EclipticLongitude(JD, AASELPMPP02.Correction.LLR), AASELPMPP02.EclipticLatitude(JD, AASELPMPP02.Correction.LLR), 2451545.0, JD);
-                    //        ExcessApparentGeocentricLongitude = AASCoordinateTransformation.MapTo0To360Range(MoonPos.X - AASSun.ApparentEclipticLongitude(JD, true));
-                    //        break;
-                    
-                    //case Algorithm.ELPMPP02DE405:
-                    //    MoonPos = AASPrecession.PrecessEcliptic(AASELPMPP02.EclipticLongitude(JD, AASELPMPP02.Correction.DE405), AASELPMPP02.EclipticLatitude(JD, AASELPMPP02.Correction.DE405), 2451545.0, JD);
-                    //        ExcessApparentGeocentricLongitude = AASCoordinateTransformation.MapTo0To360Range(MoonPos.X - AASSun.ApparentEclipticLongitude(JD, true));
-                    //        break;
-                    
-                    //case Algorithm.ELPMPP02DE406:
-                    //    MoonPos = AASPrecession.PrecessEcliptic(AASELPMPP02.EclipticLongitude(JD, AASELPMPP02.Correction.DE406), AASELPMPP02.EclipticLatitude(JD, AASELPMPP02.Correction.DE406), 2451545.0, JD);
-                    //        ExcessApparentGeocentricLongitude = AASCoordinateTransformation.MapTo0To360Range(MoonPos.X - AASSun.ApparentEclipticLongitude(JD, true));
-                    //        break;
-                    
 
                     default:
                         throw new Exception($"{algorithm} is not a valid value");
